7d9cf61694830ede98a0a129c7aee991521196cc
Prashant Raut <spashraut@gmail.com>
2020-12-19 16:56:00 +0530

    First Commit

---
+++ src/context/mockData.js/mockUser.js
@@ -0,0 +1,40 @@
+export default {
+	login: 'programmer-prashant',
+	id: 73628816,
+	node_id: 'MDQ6VXNlcjczNjI4ODE2',
+	avatar_url: 'https://avatars0.githubusercontent.com/u/73628816?v=4',
+	gravatar_id: '',
+	url: 'https://api.github.com/users/programmer-prashant',
+	html_url: 'https://github.com/programmer-prashant',
+	followers_url: 'https://api.github.com/users/programmer-prashant/followers',
+	following_url:
+		'https://api.github.com/users/programmer-prashant/following{/other_user}',
+	gists_url: 'https://api.github.com/users/programmer-prashant/gists{/gist_id}',
+	starred_url:
+		'https://api.github.com/users/programmer-prashant/starred{/owner}{/repo}',
+	subscriptions_url:
+		'https://api.github.com/users/programmer-prashant/subscriptions',
+	organizations_url: 'https://api.github.com/users/programmer-prashant/orgs',
+	repos_url: 'https://api.github.com/users/programmer-prashant/repos',
+	events_url:
+		'https://api.github.com/users/programmer-prashant/events{/privacy}',
+	received_events_url:
+		'https://api.github.com/users/programmer-prashant/received_events',
+	type: 'User',
+	site_admin: false,
+	name: 'Prashant Raut',
+	company: 'ThinkQuotient Software Private Limited',
+	blog: '',
+	location: 'india',
+	email: null,
+	hireable: null,
+	bio:
+		'Hi there! I am Prashant \r\nA passionate computer programmer and Engineer, born and brought\t\t\t\t\tup in India.\r\nI am full stack developer 🔥',
+	twitter_username: 'PPRDM77',
+	public_repos: 16,
+	public_gists: 23,
+	followers: 732,
+	following: 426,
+	created_at: '2020-10-29T04:31:15Z',
+	updated_at: '2020-12-16T12:00:31Z',
+};
--- src/index.js
+++ src/index.js
@@ -2,16 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
+import { GithubProvider } from './context/context';
+import { Auth0Provider } from '@auth0/auth0-react';
 
+//dev-o1e2oiea.us.auth0.com
+//D7hE111Z62GukczXqzG5VeTalNmiuRuA
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Auth0Provider
+			domain='dev-o1e2oiea.us.auth0.com'
+			clientId='D7hE111Z62GukczXqzG5VeTalNmiuRuA'
+			redirectUri={window.location.origin}
+			cacheLocation='localstorage'
+		>
+			<GithubProvider>
+				<App />
+			</GithubProvider>
+		</Auth0Provider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
